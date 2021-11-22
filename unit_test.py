f78741ea9a3951fc59a746e907d8e29e5d975bf2
yagaagowtham.p <yagaagowtham.p@zohocorp.com>
2021-11-22 12:12:04 +0530

    Configure unit test

--- unit_test.py
+++ unit_test.py
@@ -4,10 +4,6 @@ import calc
 class SimpleTest(unittest.TestCase):
   
     def test(self):
-        # if calc.calculate(5, 6, '-') == -2:
-        #     return True
-        # else:
-        #     return False
         assert calc.calculate(5, 6, '-') == -1
   
 if __name__ == '__main__':
